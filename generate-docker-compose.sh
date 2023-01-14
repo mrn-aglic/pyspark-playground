e622cdd648a894785c5b1f5832b96b22dfc8cbf4
Marin Aglic <mrn.aglic@gmail.com>
2023-01-14 11:49:37 +0100

    Fix datanode web interface access issue

--- generate-docker-compose.sh
+++ generate-docker-compose.sh
@@ -4,38 +4,34 @@ NUM_WORKERS=$1
 
 DOCKER_COMPOSE_NAME="docker-compose.generated.yml"
 
-#NGINX_FILE_NAME="nginx.generated.conf"
+HDFS_SITE_TEMPLATE=$(cat templates/hdfs-site.xml.tmpl)
+
+mkdir -p yarn-generated
+HDFS_FILE_TARGET_DIR="yarn-generated"
 
 port1=8042
-port2=9864
+port2=9861
 
 echo "" > $DOCKER_COMPOSE_NAME
 
 DOCKER_WORKERS_TMPL=""
-#NGINX_FULL_TMPL=""
 
 for((i=1; i <= NUM_WORKERS; i++))
 do
   DOCKER_WORKER_TMPL=$(cat templates/yarn-data-node.tmpl)
-#  NGINX_TMPL=$(cat templates/nginx.tmpl)
 
   num=$i
+  HDFS_SITE_FILE_NAME="hdfs-site.$num.xml"
+
   DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL//\{worker_num\}/$num}"
   DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL/\{port1\}/$port1}"
-  DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL/\{port2\}/$port2}"
+  DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL//\{port2\}/$port2}"
+  DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL/\{hdfs-site-file\}/$HDFS_SITE_FILE_NAME}"
 
-
-#  NGINX_TMPL="${NGINX_TMPL/\{num\}/$num}"
-#  NGINX_TMPL="${NGINX_TMPL/\{port\}/$port}"
+  HDFS_FILE_CONTENT="${HDFS_SITE_TEMPLATE/\{port\}/$port2}"
+  echo "$HDFS_FILE_CONTENT" > "$HDFS_FILE_TARGET_DIR/$HDFS_SITE_FILE_NAME"
 
   DOCKER_WORKERS_TMPL="$DOCKER_WORKERS_TMPL\n\n$DOCKER_WORKER_TMPL"
-
-#  if [ "$NGINX_FULL_TMPL" == "" ]
-#  then
-#    NGINX_FULL_TMPL="$NGINX_TMPL"
-#  else
-#    NGINX_FULL_TMPL="$NGINX_FULL_TMPL\n\n$NGINX_TMPL"
-#  fi
 
   port1=$((port1 + 1))
   port2=$((port2 + 1))
@@ -48,5 +44,3 @@ DOCKER_COMPOSE_TMPL="${DOCKER_COMPOSE_TMPL/\{nginx_conf\}/$NGINX_FILE_NAME}"
 DOCKER_COMPOSE_TMPL="${DOCKER_COMPOSE_TMPL/\{workers\}/$DOCKER_WORKERS_TMPL}"
 
 echo "$DOCKER_COMPOSE_TMPL" > $DOCKER_COMPOSE_NAME
-
-#echo "$NGINX_FULL_TMPL" > $NGINX_FILE_NAME
