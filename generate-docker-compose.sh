468b721f910fefb134ae0c96428f79ab057364df
Marin Aglic <mrn.aglic@gmail.com>
2023-01-14 12:16:02 +0100

    Support exposing nodemanager web interface

--- generate-docker-compose.sh
+++ generate-docker-compose.sh
@@ -5,6 +5,7 @@ NUM_WORKERS=$1
 DOCKER_COMPOSE_NAME="docker-compose.generated.yml"
 
 HDFS_SITE_TEMPLATE=$(cat templates/hdfs-site.xml.tmpl)
+YARN_SITE_TEMPLATE=$(cat templates/yarn-site.xml.tmpl)
 
 mkdir -p yarn-generated
 HDFS_FILE_TARGET_DIR="yarn-generated"
@@ -22,11 +23,16 @@ do
 
   num=$i
   HDFS_SITE_FILE_NAME="hdfs-site.$num.xml"
+  YARN_SITE_FILE_NAME="yarn-site.$num.xml"
 
   DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL//\{worker_num\}/$num}"
-  DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL/\{port1\}/$port1}"
+  DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL//\{port1\}/$port1}"
   DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL//\{port2\}/$port2}"
   DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL/\{hdfs-site-file\}/$HDFS_SITE_FILE_NAME}"
+  DOCKER_WORKER_TMPL="${DOCKER_WORKER_TMPL/\{yarn-site-file\}/$YARN_SITE_FILE_NAME}"
+
+  YARN_FILE_CONTENT="${YARN_SITE_TEMPLATE/\{port\}/$port1}"
+  echo "$YARN_FILE_CONTENT" > "$HDFS_FILE_TARGET_DIR/$YARN_SITE_FILE_NAME"
 
   HDFS_FILE_CONTENT="${HDFS_SITE_TEMPLATE/\{port\}/$port2}"
   echo "$HDFS_FILE_CONTENT" > "$HDFS_FILE_TARGET_DIR/$HDFS_SITE_FILE_NAME"
