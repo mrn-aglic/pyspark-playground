8682c9b20035c83e2cd97d3cbbc9a2739708f210
Marin Aglic <mrn.aglic@gmail.com>
2023-01-23 09:48:09 +0100

    Chapter 07 exercises

---
+++ spark_apps/data_analysis_book/chapter07/ex7.2.py
@@ -0,0 +1,73 @@
+from functools import reduce
+
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName(
+    "Backblaze data - Blending SQL and Python"
+).getOrCreate()
+
+data_dir = "/opt/spark/data/backblaze_data"
+
+DATA_FILES = ["data_Q3_2019"]
+
+data = [
+    spark.read.csv(f"{data_dir}/{file_dir}", header=True, inferSchema=True)
+    for file_dir in DATA_FILES
+]
+
+common_columns = list(
+    reduce(
+        lambda acc, element: acc.intersection(element), [set(df.columns) for df in data]
+    )
+)
+
+assert {"model", "capacity_bytes", "date", "failure"}.issubset(set(common_columns))
+
+full_data = reduce(
+    lambda acc, df: acc.select(common_columns).union(df.select(common_columns)), data
+)
+
+# full_data.printSchema()
+
+# Methods that accept SQL-type statements:
+# selectExpr, epxr, where/filter
+# selectExpr() is just like the select() method with the exception that it will pro- cess SQL-style operations.
+
+full_data = full_data.selectExpr(
+    "model", "(capacity_bytes / pow(1024, 3)) as capacity_GB", "date", "failure"
+)
+
+# Alternative:
+# full_data = full_data.select(
+#     F.col("model"),
+#     (F.col("capacity_bytes") / F.pow(F.lit(1024), 3)).alias("capacity_GB"),
+#     F.col("date"),
+#     F.col("failure")
+# )
+
+drive_days = full_data.groupby("model", "capacity_GB").agg(
+    F.count("*").alias("drive_days")
+)
+
+failures = (
+    full_data.where("failure = 1")
+    .groupby("model", "capacity_GB")
+    .agg(F.count("*").alias("failures"))
+)
+
+# Alternative
+# failures = (
+#     full_data.where("failure = 1")
+#     .groupby("model", "capacity_GB")
+#     .agg(F.expr("count(*) failures"))
+# )
+
+failures.show(5)
+
+summarized_data = (
+    drive_days.join(failures, on=["model", "capacity_GB"], how="left")
+    .fillna(0.0, "failures")
+    .selectExpr("model", "capacity_GB", "(failures / drive_days) AS failure_rate")
+    .cache()
+)
