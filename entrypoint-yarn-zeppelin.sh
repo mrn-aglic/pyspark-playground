9ca942c3448d747558eabbd123db4fbdca8695fb
Kevin Williams Permana <kevinity310@gmail.com>
2023-12-30 13:52:07 +0700

    fix zeppelin automate running in container in hadoop

---
+++ entrypoint-yarn-zeppelin.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+SPARK_WORKLOAD=$1
+
+echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
+
+/etc/init.d/ssh start
+
+if [ "$SPARK_WORKLOAD" == "master" ];
+then
+  hdfs namenode -format
+
+  # start the master node processes
+  hdfs --daemon start namenode
+  hdfs --daemon start secondarynamenode
+  yarn --daemon start resourcemanager
+
+  # create required directories
+  while ! hdfs dfs -mkdir -p /spark-logs;
+  do
+    echo "Failed creating /spark-logs hdfs dir"
+  done
+  echo "Created /spark-logs hdfs dir"
+  hdfs dfs -mkdir -p /opt/spark/data
+  echo "Created /opt/spark/data hdfs dir"
+
+
+  # copy the data to the data HDFS directory
+  hdfs dfs -copyFromLocal /opt/spark/data/* /opt/spark/data
+  hdfs dfs -ls /opt/spark/data
+
+  zeppelin-daemon.sh start
+
+elif [ "$SPARK_WORKLOAD" == "worker" ];
+then
+  hdfs namenode -format
+
+  # start the worker node processes
+  hdfs --daemon start datanode
+  yarn --daemon start nodemanager
+elif [ "$SPARK_WORKLOAD" == "history" ];
+then
+
+  while ! hdfs dfs -test -d /spark-logs;
+  do
+    echo "spark-logs doesn't exist yet... retrying"
+    sleep 1;
+  done
+  echo "Exit loop"
+
+  # start the spark history server
+  start-history-server.sh
+fi
+
+tail -f /dev/null
+
